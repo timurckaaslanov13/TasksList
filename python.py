04131f885d66e677122b220162c429c57cd18a5b
timurchik <timurckaaslanov@yandex.ru>
2024-02-07 22:20:32 +0300

    Create app TasksList

---
+++ python.py
@@ -0,0 +1,128 @@
+class Task:
+    """Класс хранит информацию о задаче """
+    
+    def __init__(self, name: str, description: str, status: bool = False) -> None:
+        """Инициализация объекта класса
+
+        Args:
+            name (str): _description_
+            description (str): _description_
+            status (bool, optional): _description_. Defaults to False.
+        """
+        self.name = name
+        self.description = description
+        self.status = status
+        
+    def display(self) -> None:
+        """Вывод всех задач в консоль
+        """
+        print(f'{self.name} (Сделана)') if self.status else print(f'{self.name} (Не сделана)')
+        
+class TaskList:
+    """Класс содержит список задач и методы для добавления/удаления задач
+    """
+    
+    def __init__(self) -> None:
+        """Инициализация экземпляра класса
+        """
+        self.tasks = []
+        
+    def add_task(self, task: str) -> None:
+        """Добавление задач в список
+
+        Args:
+            task (str): _description_
+        """
+        self.tasks.append(task)
+        
+    def remove_task(self, task: str) -> None:
+        """Удаление задач из списка
+
+        Args:
+            task (str): _description_
+        """
+        self.tasks.remove(task)
+    
+class TaskManager:
+    """Cодержит методы для отображения списка задач и изменения статуса выполнения задач"""
+    
+    def __init__(self, task_list: TaskList) -> None:
+        """Инициализация объекта класса
+
+        Args:
+            task_list (TaskList): _description_
+        """
+        self.task_list = task_list
+        
+    def mark_done(self, task: Task) -> None:
+        """Изменение статуса выполнения задачи на положительный
+
+        Args:
+            task (Task): _description_
+        """
+        task.status = True
+        
+    def mark_undone(self, task: Task) -> None:
+        """Изменение статуса выполнения задачи на отрицательный
+
+        Args:
+            task (Task): _description_
+        """
+        task.status = False
+        
+        
+    def show_tasks(self) -> None:
+        """Вывод всех задач списка в консоль
+        """
+        for i in self.task_list.tasks:
+            i.display()
+
+
+# Ниже код для проверки классов Task, TaskList и TaskManager
+
+# Создаем список задач
+todo = TaskList()
+assert todo.tasks == []
+
+# Создаем несколько задач и добавляем их в список
+task1 = Task("Стирка", "Постирать трусы, носки, слюнявчики")
+assert task1.name == 'Стирка'
+assert task1.description == 'Постирать трусы, носки, слюнявчики'
+assert task1.status is False
+task1.display()
+
+todo.add_task(task1)
+assert len(todo.tasks) == 1
+
+task2 = Task("Продукты", "Купить лук чеснок огурцы хлеб и биток")
+assert task2.name == 'Продукты'
+assert task2.description == 'Купить лук чеснок огурцы хлеб и биток'
+assert task2.status is False
+
+todo.add_task(task2)
+assert len(todo.tasks) == 2
+
+# Создаем менеджер задач и показываем список задач
+manager = TaskManager(todo)
+assert isinstance(manager.task_list, TaskList)
+print('-----Список дел-----')
+manager.show_tasks()
+
+# Отмечаем первую задачу выполненной и показываем список задач
+manager.mark_done(task1)
+
+# Проверяем изменился ли статус 2мя способами
+assert task1.status is True
+assert manager.task_list.tasks[0].status is True
+
+print('-----Список дел-----')
+manager.show_tasks()
+
+# Удаляем вторую задачу и показываем список задач
+todo.remove_task(task2)
+
+assert len(todo.tasks) == 1
+assert len(manager.task_list.tasks) == 1
+
+print('-----Список дел-----')
+manager.show_tasks()
